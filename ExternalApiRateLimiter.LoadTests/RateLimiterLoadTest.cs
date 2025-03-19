4034055e850ce247db5ba7b3b0a072e4aa3b1d0a
bdaynard <brycedaynard@live.com>
2025-03-18 19:35:18 -0700

    Missed commit to load test project

--- ExternalApiRateLimiter.LoadTests/RateLimiterLoadTest.cs
+++ ExternalApiRateLimiter.LoadTests/RateLimiterLoadTest.cs
@@ -1,27 +1,63 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
 using NBomber.CSharp;
 using NBomber.Http.CSharp;
-using System.Text.Json;
-using System.Text;
-using System.Net.Http;
 
 public class RateLimiterLoadTest
 {
     public static void Run()
     {
-        var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:5000") };
-        var phoneNumber = "+1234567890";
-        var message = "{\"message\":\"Hello\"}";
-        
-        var step = Step.Create("send_sms", async context =>
+        using var httpClient = new HttpClient();
+
+        const string Url = "http://localhost:5000/ThirdParty/sms/";
+
+        // Background scenario to simulate random numbers being consistently sent to the service.
+        var globalScenario = Scenario.Create("global_limit_scenario", async context =>
         {
-            var content = new StringContent(message, Encoding.UTF8, "application/json");
-            var response = await httpClient.PostAsync($"/ThirdParty/sms/{phoneNumber}", content);
-            return response.IsSuccessStatusCode ? Response.Ok() : Response.Fail();
-        });
+            var randomPhoneNumber = context.Random.Next(100000000, 999999999);
+            var requestBody = new StringContent("\"message\"", Encoding.UTF8, "application/json");
 
-        var scenario = ScenarioBuilder.CreateScenario("rate_limit_test", step)
-            .WithLoadSimulations(Simulation.InjectPerSec(rate: 5, during: TimeSpan.FromSeconds(30))); // Adjust rate
+            var request = Http.CreateRequest("POST", $"{Url}{randomPhoneNumber}")
+                .WithHeader("Accept", "application/json")
+                .WithHeader("Content-Type", "text/plain")
+                .WithBody(requestBody);
 
-        NBomberRunner.RegisterScenarios(scenario).Run();
+            var response = await Http.Send(httpClient, request);
+
+            return response;
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.Inject(rate: 1000,
+                              interval: TimeSpan.FromSeconds(1),
+                              during: TimeSpan.FromSeconds(60))
+        );
+
+        // Test scenario to trigger the request limit for a single number over the course of one minute.
+        var specificNumberScenario = Scenario.Create("static_number_scenario", async context =>
+        {
+            var staticPhoneNumber = 123456789;
+            var requestBody = new StringContent("\"message\"", Encoding.UTF8, "application/json");
+
+            var request = Http.CreateRequest("POST", $"{Url}{staticPhoneNumber}")
+                .WithHeader("Accept", "application/json")
+                .WithHeader("Content-Type", "text/plain")
+                .WithBody(requestBody);
+
+            var response = await Http.Send(httpClient, request);
+
+            return response;
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.RampingInject(rate: 150,
+                                     interval: TimeSpan.FromSeconds(1),
+                                     during: TimeSpan.FromSeconds(60))
+        );
+
+        NBomberRunner.RegisterScenarios(globalScenario, specificNumberScenario).Run();
     }
 }
